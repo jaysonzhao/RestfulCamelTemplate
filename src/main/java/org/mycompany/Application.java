ee31f012b3e1cf7838a047be25fa7f0208da1f13
jaysonzhao <jaysonzhao@vip.qq.com>
2019-10-07 23:14:33 +0800

    rest work

--- src/main/java/org/mycompany/Application.java
+++ src/main/java/org/mycompany/Application.java
@@ -15,9 +15,17 @@
  */
 package org.mycompany;
 
+import org.apache.activemq.jms.pool.PooledConnectionFactory;
+import org.apache.camel.component.amqp.AMQPComponent;
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.apache.qpid.jms.JmsConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+
+
 
 /**
  * A spring-boot application that includes a Camel route builder to setup the Camel routes
@@ -30,5 +38,24 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    @Bean
+    ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean servlet = new ServletRegistrationBean(
+            new CamelHttpTransportServlet(), "/rest/*");
+        servlet.setName("CamelServlet");
+        return servlet;
+    }
+    
+    @Bean(name = "amqp-component")
+    AMQPComponent amqpComponent(AMQPConfiguration config) {
+        JmsConnectionFactory qpid = new JmsConnectionFactory(config.getUsername(), config.getPassword(), "amqp://"+ config.getHost() + ":" + config.getPort());
+        //qpid.setTopicPrefix("topic://");
 
+        PooledConnectionFactory factory = new PooledConnectionFactory();
+        factory.setConnectionFactory(qpid);
+
+        return new AMQPComponent(factory);
+    }
+
+    
 }
